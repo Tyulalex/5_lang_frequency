3bac4260d74b581be7b64c18e705a2971d23842f
Tatiana Karabut <tyulalex@gmail.com>
2018-02-16 01:09:43 +0300

    lang_freq script

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Script to analyse frequency of words of a given text
+takes --filepath as argument to file with text
+and output the first top 10 frequent words in this text
+
+# System requirements
+
+Python 3 version shall be installed
+
+# Code Example
+
+e.g. **test.txt** contains the following text:
+```
+To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take arms against a sea of troubles
+```
+then when we run the script
+```
+python lang_frequency.py test.txt
+```
+
+output:
+```
+to - Occured 4 times
+the - Occured 3 times
+of - Occured 2 times
+be - Occured 2 times
+or - Occured 2 times
+suffer - Occured 1 times
+in - Occured 1 times
+fortune - Occured 1 times
+troubles - Occured 1 times
+arrows - Occured 1 times
+Script has finished its work
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,59 @@
+import argparse
+import os.path
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+def load_raw_data_from_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.readlines()
 
 
-def get_most_frequent_words(text):
-    pass
+def word_generator(raw_lines):
+    word_hyphenation = None
+    for line in raw_lines:
+        line = line.strip()
+        if line:
+            if word_hyphenation:
+                line = '{}{}'.format(word_hyphenation, line)
+            words_in_line = line.split()
+            if words_in_line[-1].endswith('-'):
+                word_hyphenation = words_in_line[-1][:-1]
+                splitted_words_in_line = words_in_line[:-1]
+            else:
+                word_hyphenation = None
+                splitted_words_in_line = words_in_line
+            for word in splitted_words_in_line:
+                yield word
+
+
+def get_parsed_words_list(raw_lines):
+    parsed_words_list = []
+    word_gen = word_generator(raw_lines)
+    for word in word_gen:
+        word = word.lower().strip('!?,.;:')
+        if word.isalpha():
+            parsed_words_list.append(word)
+    return parsed_words_list
+
+
+def get_most_frequent_words(raw_list_of_words, top_limit=10):
+    words_counter = Counter(raw_list_of_words)
+    return words_counter.most_common(top_limit)
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filepath',
+                        help="Path to txt file")
+    args = parser.parse_args()
+    if os.path.isfile(args.filepath):
+        raw_data = load_raw_data_from_file(args.filepath)
+        parsed_words_list = get_parsed_words_list(raw_data)
+        most_frequent_words = get_most_frequent_words(parsed_words_list)
+        print("most frequent words:\n")
+        for frequent_word in most_frequent_words:
+            print("{} - Occured {} times".format(frequent_word[0],
+                                                 frequent_word[1]))
+    else:
+        print("filepath not found")
+    print("Script has finished its work")
